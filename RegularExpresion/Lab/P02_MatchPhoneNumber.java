e5348c8ac817eadae6fb03f20bdc303b7e49fa5a
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-06-16 13:47:32 +0300

    Update P02_MatchPhoneNumber.java

--- RegularExpresion/Lab/P02_MatchPhoneNumber.java
+++ RegularExpresion/Lab/P02_MatchPhoneNumber.java
@@ -1,7 +1,28 @@
 package ProgrammingFundamentals.RegularExpresion.Lab;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P02_MatchPhoneNumber {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<String> phoneNumbers = new ArrayList<>();
+
+        String regex = "\\+359+(?<separator>[ -])2\\1[0-9]{3}\\1[0-9]{4}\\b";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            phoneNumbers.add(matcher.group());
+        }
+
+        System.out.print(String.join(", ",phoneNumbers));
+
+
     }
 }
