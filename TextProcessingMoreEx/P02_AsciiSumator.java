b7bdd108eb815065ee23d313eb330e51435d5d9d
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-07-27 20:59:26 +0300

    Fundamentals

--- TextProcessingMoreEx/P02_AsciiSumator.java
+++ TextProcessingMoreEx/P02_AsciiSumator.java
@@ -1,4 +1,24 @@
 package ProgrammingFundamentals.TextProcessingMoreEx;
 
+import java.util.Scanner;
+
 public class P02_AsciiSumator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char firstSymbol = scanner.nextLine().charAt(0);
+
+        char secondSymbol = scanner.nextLine().charAt(0);
+
+        String randomText = scanner.nextLine();
+
+        int minValue = Math.min(firstSymbol, secondSymbol);
+        int maxValue = Math.max(firstSymbol, secondSymbol);
+        int totalSum = 0;
+        for (char symbol : randomText.toCharArray()) {
+            if (symbol > minValue && symbol < maxValue){
+                totalSum+=symbol;
+            }
+        }
+        System.out.println(totalSum);
+    }
 }
