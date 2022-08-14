9d00177515b013c92641f3f22cff7e4f3002c40f
Plamen Yordanov <plamenyordanov91@gmail.com>
2022-08-14 14:14:37 +0300

    Fundamentals

--- ObjectAndClasses/MoreExercises/P01_CompanyRoster/Employee.java
+++ ObjectAndClasses/MoreExercises/P01_CompanyRoster/Employee.java
@@ -1,15 +1,14 @@
 package ProgrammingFundamentals.ObjectAndClasses.MoreExercises.P01_CompanyRoster;
 
-public class Employees {
-
+public class Employee {
     private String name;
     private double salary;
     private String position;
     private String department;
     private String email;
-    private  String age;
+    private String age;
 
-    public Employees(String name, double salary, String position, String department, String email, String age){
+    public Employee(String name, double salary, String position, String department, String email, String age) {
         this.name = name;
         this.salary = salary;
         this.position = position;
@@ -18,27 +17,52 @@ public class Employees {
         this.age = age;
     }
 
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public double getSalary() {
         return salary;
     }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     public String getPosition() {
         return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
     }
 
     public String getDepartment() {
         return department;
     }
 
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getAge() {
         return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
--- ObjectAndClasses/MoreExercises/P01_CompanyRoster/Main.java
+++ ObjectAndClasses/MoreExercises/P01_CompanyRoster/Main.java
@@ -1,83 +1,56 @@
 package ProgrammingFundamentals.ObjectAndClasses.MoreExercises.P01_CompanyRoster;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Employees> employeesList = new ArrayList<>();
-
-
         int n = Integer.parseInt(scanner.nextLine());
+       Map<String, List<Employee>> mapDepartments =new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] inputLine = scanner.nextLine().split("\\s+");
+            String name = inputLine[0];
+            double salary = Double.parseDouble(inputLine[1]);
+            String position = inputLine[2];
+            String department = inputLine[3];
+            String email = "";
+            String age = "";
 
-        for (int i = 1; i <= n; i++) {
-
-            String inputLine = scanner.nextLine();
-            String name = inputLine.split(" ")[0];
-            double salary = Double.parseDouble(inputLine.split(" ")[1]);
-            String position = inputLine.split(" ")[2];
-            String department = inputLine.split(" ")[3];
-
-            if (inputLine.split(" ").length == 6) {
-                String email = inputLine.split(" ")[4];
-                String age = (inputLine.split(" ")[5]);
-                Employees departmentsObj = new Employees(name, salary, position, department, email, age);
-                employeesList.add(departmentsObj);
-
-            } else if (inputLine.split(" ").length == 4) {
-
-                String email = "n/a";
-                String age = "-1";
-                Employees departmentsObj = new Employees(name, salary, position, department, email, age);
-                employeesList.add(departmentsObj);
-            } else {
-                boolean hasItEmail = false;
-                String emailOrAge = inputLine.split(" ")[4];
-                for (char element : emailOrAge.toCharArray()) {
-                    if (element == '@') {
-                        hasItEmail = true;
-                        break;
-                    }
-                }
-                if (hasItEmail) {
-                    String email = inputLine.split(" ")[4];
-                    String age = "-1";
-                    Employees departmentsObj = new Employees(name, salary, position, department, email, age);
-                    employeesList.add(departmentsObj);
+            if (inputLine.length == 4) {
+                email = "n/a";
+                age = "-1";
+            } else if (inputLine.length == 5) {
+                if (inputLine[4].contains("@")) {
+                    email = inputLine[4];
+                    age = "-1";
                 } else {
-                    String age = inputLine.split(" ")[4];
-                    String email = "n/a";
-                    Employees departmentsObj = new Employees(name, salary, position, department, email, age);
-                    employeesList.add(departmentsObj);
+                    email = "n/a";
+                    age = inputLine[4];
                 }
-
+            } else {
+                email = inputLine[4];
+                age = inputLine[5];
             }
-
+            Employee objEmployee = new Employee(name,salary,position,department,email,age);
+            mapDepartments.putIfAbsent(department,new ArrayList<>());
+            mapDepartments.get(department).add(objEmployee);
         }
-        List<String> departmentsList = employeesList.stream().map(Employees::getDepartment).distinct().collect(Collectors.toList());
+        System.out.println();
 
-        List<Departments> departments = new ArrayList<>();
+        mapDepartments.entrySet().stream().sorted((a,b) -> {
+            double averageFirst = a.getValue().stream().mapToDouble(Employee::getSalary).average().orElse(Double.NaN);
+            double averageSecond = b.getValue().stream().mapToDouble(Employee::getSalary).average().orElse(Double.NaN);
+            return Double.compare(averageSecond,averageFirst);
+        }).limit(1).forEach(e ->{
+            System.out.println(String.format("Highest Average Salary: %s",e.getKey()));
+            e.getValue().stream().sorted((a,b) -> Double.compare(b.getSalary(), a.getSalary())).forEach(o ->
+                    System.out.println(String.format("%s %.2f %s %s", o.getName(), o.getSalary(),o.getEmail(),o.getAge())));
+        });
+        System.out.println();
 
-        for (String department : departmentsList) {
 
-            departments.add(new Departments(department,
-                    employeesList.stream().filter(employees -> employees.getDepartment().equals(department)).collect(Collectors.toList())));
-        }
-        departments.sort(Comparator.comparing(Departments::getAvgSalary).reversed());
-        Departments department = departments.get(0);
-
-        department.getEmployee().sort(Comparator.comparing(Employees::getSalary).reversed());
-        System.out.printf("Highest Average Salary: %s%n",department.getName());
-
-        for (Employees employees : department.getEmployee()) {
-            System.out.printf("%s %.2f %s %s%n", employees.getName(), employees.getSalary(), employees.getEmail(), employees.getAge());
-            
-        }
 
 
     }
